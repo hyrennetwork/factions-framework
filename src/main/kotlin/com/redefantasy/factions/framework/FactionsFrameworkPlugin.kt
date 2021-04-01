e78655794084ba3ee42cd8598aa033819090f16d
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-01 06:13:26 -0300

    dev

--- src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
+++ src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
@@ -56,7 +56,7 @@ class FactionsFrameworkPlugin : CustomPlugin(false) {
                                 }
                             }
 
-                            packet.b = players
+                            packet.b = players.toHashSet()
                         }
                     } catch (e: Exception) {
                         e.printStackTrace()
