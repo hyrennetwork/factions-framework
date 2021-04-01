68c9d00d35e774b20387fd8ed8efb87930ac5a75
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-01 00:42:08 -0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    kotlin("jvm") version "1.4.31"
+
+    id("maven-publish")
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+}
+
+group = "com.redefantasy"
+version = "0.1-ALPHA"
+
+repositories {
+    mavenCentral()
+
+    mavenLocal()
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+
+    shadowJar {
+        val fileName = "${project.name}.jar"
+
+        archiveFileName.set("${project.name}.jar")
+
+        doLast {
+            try {
+                val file = file("build/libs/$fileName")
+
+                val toDelete = file("/home/cloud/output/$fileName")
+
+                if (toDelete.exists()) toDelete.delete()
+
+                file.copyTo(file("/home/cloud/output/$fileName"))
+                file.delete()
+            } catch (ex: java.io.FileNotFoundException) {
+                ex.printStackTrace()
+            }
+        }
+    }
+}
+
+dependencies {
+    // kotlin
+    implementation(kotlin("stdlib"))
+
+    // paperspigot
+    compileOnly("org.github.paperspigot:paperspigot:1.8.8-R0.1-SNAPSHOT")
+
+    // waterfall chat
+    compileOnly("io.github.waterfallmc:waterfall-chat:1.16-R0.5-SNAPSHOT")
+
+    // eventbus
+    compileOnly("org.greenrobot:eventbus:3.2.0")
+
+    // caffeine
+    compileOnly("com.github.ben-manes.caffeine:caffeine:2.8.5")
+
+    // core
+    compileOnly("com.redefantasy:core-shared:0.1-ALPHA")
+    compileOnly("com.redefantasy:core-spigot:0.1-ALPHA")
+}
---
+++ src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
@@ -0,0 +1,43 @@
+package com.redefantasy.factions.framework
+
+import com.redefantasy.core.spigot.CoreSpigotConstants
+import com.redefantasy.core.spigot.misc.plugin.CustomPlugin
+import com.redefantasy.core.spigot.misc.utils.PacketEvent
+import com.redefantasy.core.spigot.misc.utils.PacketListener
+import com.redefantasy.core.spigot.misc.utils.ProtocolHandler
+import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo
+
+/**
+ * @author Gutyerrez
+ */
+class FactionsFrameworkPlugin : CustomPlugin(false) {
+
+    override fun onEnable() {
+        super.onEnable()
+
+        CoreSpigotConstants.PROTOCOL_HANDLER.registerListener(
+            object : PacketListener() {
+
+                override fun onSent(
+                    event: PacketEvent
+                ) {
+                    val packet = event.packet
+
+                    println("opa")
+
+                    if (packet is PacketPlayOutPlayerInfo) {
+                        println("daleee")
+                    }
+                }
+
+            }
+        )
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+
+        CoreSpigotConstants.PROTOCOL_HANDLER.close()
+    }
+
+}
