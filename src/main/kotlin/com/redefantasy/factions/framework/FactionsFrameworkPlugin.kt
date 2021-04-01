630281c5e96d1b4b8d735c9de9ebf5055a034aac
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-01 05:56:59 -0300

    dev

--- src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
+++ src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
@@ -5,6 +5,7 @@ import com.redefantasy.core.spigot.CoreSpigotConstants
 import com.redefantasy.core.spigot.misc.plugin.CustomPlugin
 import com.redefantasy.core.spigot.misc.utils.PacketEvent
 import com.redefantasy.core.spigot.misc.utils.PacketListener
+import net.minecraft.server.v1_8_R3.ChatComponentText
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo.EnumPlayerInfoAction
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo.PlayerInfoData
@@ -69,11 +70,13 @@ class FactionsFrameworkPlugin : CustomPlugin(false) {
                     return PlayerInfoData(
                         GameProfile(
                             UUID.randomUUID(),
-                            "${text}_Z"
+                            if (text.length > 16) null else text
                         ),
                         i,
                         WorldSettings.EnumGamemode.SURVIVAL,
-                        null
+                        ChatComponentText(
+                            if (text.length <= 16) null else text
+                        )
                     )
                 }
 
