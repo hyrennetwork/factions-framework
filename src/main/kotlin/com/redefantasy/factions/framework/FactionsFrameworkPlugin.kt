1506b1cf7235357819c70b1e1a4d5ea7bffde1b9
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-01 01:47:08 -0300

    dev

--- src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
+++ src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
@@ -1,10 +1,14 @@
 package com.redefantasy.factions.framework
 
+import com.mojang.authlib.GameProfile
 import com.redefantasy.core.spigot.CoreSpigotConstants
 import com.redefantasy.core.spigot.misc.plugin.CustomPlugin
 import com.redefantasy.core.spigot.misc.utils.PacketEvent
 import com.redefantasy.core.spigot.misc.utils.PacketListener
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo
+import net.minecraft.server.v1_8_R3.WorldSettings
+import org.apache.commons.lang.RandomStringUtils
+import java.util.*
 
 /**
  * @author Gutyerrez
@@ -23,7 +27,19 @@ class FactionsFrameworkPlugin : CustomPlugin(false) {
                     val packet = event.packet
 
                     if (packet is PacketPlayOutPlayerInfo) {
-                        println(packet)
+                        val playerInfoData = PacketPlayOutPlayerInfo.PlayerInfoData(
+                            GameProfile(
+                                UUID.randomUUID(),
+                                RandomStringUtils.random(9)
+                            ),
+                            0,
+                            WorldSettings.EnumGamemode.SURVIVAL,
+                            null
+                        )
+
+                        for (i in 0 until 10) {
+                            packet.b.add(playerInfoData)
+                        }
                     }
                 }
 
