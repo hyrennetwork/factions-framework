1eafc92bd33feb13bf65477cbf1ed0d59b63b511
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-01 06:40:32 -0300

    dev

--- src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
+++ src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
@@ -5,12 +5,10 @@ import com.redefantasy.core.spigot.CoreSpigotConstants
 import com.redefantasy.core.spigot.misc.plugin.CustomPlugin
 import com.redefantasy.core.spigot.misc.utils.PacketEvent
 import com.redefantasy.core.spigot.misc.utils.PacketListener
-import net.minecraft.server.v1_8_R3.ChatComponentText
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo.EnumPlayerInfoAction
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo.PlayerInfoData
 import net.minecraft.server.v1_8_R3.WorldSettings
-import org.apache.commons.lang3.RandomStringUtils
 import java.util.*
 
 /**
@@ -64,20 +62,18 @@ class FactionsFrameworkPlugin : CustomPlugin(false) {
                 }
 
                 private fun createPlayerInfoDataFromText(text: String, i: Int = 0): PlayerInfoData {
-                    if (text.length > 32) throw IllegalArgumentException(
-                        "\"$text\" length is higher than 32!"
-                    )
-
                     return PlayerInfoData(
                         GameProfile(
                             UUID.randomUUID(),
-                            if (text.length > 16) RandomStringUtils.randomAlphabetic(16) else text
+//                            if (text.length > 32) RandomStringUtils.randomAlphabetic(16) else text
+                            text
                         ),
                         i,
                         WorldSettings.EnumGamemode.SURVIVAL,
-                        if (text.length <= 16) null else ChatComponentText(
-                            text
-                        )
+//                        if (text.length <= 16) null else ChatComponentText(
+//                            text
+//                        )
+                        null
                     )
                 }
 
