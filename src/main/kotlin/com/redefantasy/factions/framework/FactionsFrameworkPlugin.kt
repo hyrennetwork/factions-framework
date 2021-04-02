78f64ebb702b80b5453d89db63ceb1a08247e278
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-02 03:17:10 -0300

    dev

--- src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
+++ src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
@@ -1,26 +1,22 @@
 package com.redefantasy.factions.framework
 
 import com.redefantasy.core.spigot.CoreSpigotConstants
-import com.redefantasy.core.spigot.misc.player.sendPacket
 import com.redefantasy.core.spigot.misc.plugin.CustomPlugin
-import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo
-import net.minecraft.server.v1_8_R3.WorldSettings
+import com.redefantasy.factions.framework.misc.tablist.PlayerList
 import org.bukkit.Bukkit
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
-import java.util.*
 
 /**
  * @author Gutyerrez
  */
 class FactionsFrameworkPlugin : CustomPlugin(false) {
 
-    val RANDOM = Random()
-
     override fun onEnable() {
         super.onEnable()
+
+        val playerList = PlayerList(null, PlayerList.SIZE_FOUR)
 
         val pluginManager = Bukkit.getServer().pluginManager
 
@@ -33,34 +29,7 @@ class FactionsFrameworkPlugin : CustomPlugin(false) {
                 ) {
                     val player = event.player
 
-                    val tabs = java.lang.reflect.Array.newInstance(
-                        String::class.java,
-                        80
-                    )
 
-                    // Clear players
-
-                    var packet = PacketPlayOutPlayerInfo()
-
-                    packet.a = PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER
-
-                    Bukkit.getOnlinePlayers().forEach {
-                        val craftPlayer = it as CraftPlayer
-                        val gameProfile = craftPlayer.profile
-
-                        val playerInfoData = PacketPlayOutPlayerInfo.PlayerInfoData(
-                            gameProfile,
-                            0,
-                            WorldSettings.EnumGamemode.NOT_SET,
-                            null
-                        )
-
-                        packet.b.add(playerInfoData )
-                    }
-
-                    player.sendPacket(packet)
-
-                    // Clear players
                 }
 
             },
