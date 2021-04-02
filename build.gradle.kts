b7fef613a4c73038c3f6af57161ecde70db5a027
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-02 03:32:04 -0300

    dev

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     kotlin("jvm") version "1.4.31"
 
-    id("java")
-    id("java-library")
+    id("application")
     id("maven-publish")
     id("com.github.johnrengelman.shadow") version "6.1.0"
 }
