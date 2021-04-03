a800cc1e7668135d37775437f8c62ae68084a868
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-03 01:57:02 -0300

    dev

--- src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
+++ src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
@@ -1,12 +1,17 @@
 package com.redefantasy.factions.framework
 
+import com.mojang.authlib.GameProfile
+import com.redefantasy.core.shared.misc.utils.SequencePrefix
 import com.redefantasy.core.spigot.CoreSpigotConstants
 import com.redefantasy.core.spigot.misc.plugin.CustomPlugin
 import com.redefantasy.core.spigot.misc.utils.PacketEvent
 import com.redefantasy.core.spigot.misc.utils.PacketListener
 import com.redefantasy.factions.framework.misc.tablist.PlayerList
+import net.minecraft.server.v1_8_R3.ChatComponentText
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo
-import org.apache.commons.lang3.RandomStringUtils
+import net.minecraft.server.v1_8_R3.WorldSettings
+import org.bukkit.craftbukkit.v1_8_R3.util.CraftChatMessage
+import java.util.*
 
 /**
  * @author Gutyerrez
@@ -26,19 +31,47 @@ class FactionsFrameworkPlugin : CustomPlugin(false) {
                     val packet = event.packet
 
                     if (packet is PacketPlayOutPlayerInfo && !packet.channels.contains(PlayerList.CHANNEL_NAME)) {
-                        val playerList = PlayerList.getPlayerList(player)
+                        val packet = PacketPlayOutPlayerInfo()
 
-                        playerList.update(0, "§e§lMINHA FACÇÃO")
-                        playerList.update(1, "§e[STF] STAFF")
-                        playerList.update(2, "§1")
-                        playerList.update(3, "§a• §6[Master] #Gutyerrez")
-                        playerList.update(4, "§a• §6[Master] *ImRamon")
-                        playerList.update(5, "§7• [Master] +VICTORBBBBR")
+                        packet.a = PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER
 
-                        for (i in 6 until 16)
-                            playerList.update(i, "§7• -${RandomStringUtils.randomAlphabetic(8)}")
+                        packet.channels.add(PlayerList.CHANNEL_NAME)
+                        packet.b = MutableList(80) {
+                            PacketPlayOutPlayerInfo.PlayerInfoData(
+                                GameProfile(
+                                    UUID.randomUUID(),
+                                    "__${SequencePrefix().next()}"
+                                ),
+                                0,
+                                WorldSettings.EnumGamemode.SURVIVAL,
+                                CraftChatMessage.fromString(
+//                                    this.generateHiddenString()
+                                    "§0"
+                                )[0]
+                            )
+                        }
 
-                        event.packet = playerList.PACKET
+                        val playerInfoData = packet.b[0]
+
+                        val field = playerInfoData::class.java.getDeclaredField("e")
+
+                        field.isAccessible = true
+
+                        field.set(playerInfoData, ChatComponentText("§e§lMINHA FACÇÃO"))
+
+//                        val playerList = PlayerList.getPlayerList(player)
+
+//                        playerList.update(0, "§e§lMINHA FACÇÃO")
+//                        playerList.update(1, "§e[STF] STAFF")
+//                        playerList.update(2, "§1")
+//                        playerList.update(3, "§a• §6[Master] #Gutyerrez")
+//                        playerList.update(4, "§a• §6[Master] *ImRamon")
+//                        playerList.update(5, "§7• [Master] +VICTORBBBBR")
+
+//                        for (i in 6 until 16)
+//                            playerList.update(i, "§7• -${RandomStringUtils.randomAlphabetic(8)}")
+
+                        event.packet = packet
                     }
                 }
 
