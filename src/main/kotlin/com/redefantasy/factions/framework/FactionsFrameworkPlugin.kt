614b670f558518e0a4b44404bb2ed43d90fe5407
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-03 01:19:04 -0300

    dev

--- src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
+++ src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
@@ -2,10 +2,7 @@ package com.redefantasy.factions.framework
 
 import com.redefantasy.core.spigot.CoreSpigotConstants
 import com.redefantasy.core.spigot.misc.plugin.CustomPlugin
-import com.redefantasy.core.spigot.misc.utils.PacketEvent
-import com.redefantasy.core.spigot.misc.utils.PacketListener
 import com.redefantasy.factions.framework.misc.tablist.PlayerList
-import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo
 import org.apache.commons.lang3.RandomStringUtils
 import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
@@ -50,7 +47,7 @@ class FactionsFrameworkPlugin : CustomPlugin(false) {
             this
         )
 
-        CoreSpigotConstants.PROTOCOL_HANDLER.registerListener(
+        /*CoreSpigotConstants.PROTOCOL_HANDLER.registerListener(
             object : PacketListener() {
 
                 override fun onSent(
@@ -73,7 +70,7 @@ class FactionsFrameworkPlugin : CustomPlugin(false) {
                 }
 
             }
-        )
+        )*/
     }
 
     override fun onDisable() {
--- src/main/kotlin/com/redefantasy/factions/framework/misc/tablist/PlayerList.kt
+++ src/main/kotlin/com/redefantasy/factions/framework/misc/tablist/PlayerList.kt
@@ -1,6 +1,7 @@
 package com.redefantasy.factions.framework.misc.tablist
 
 import com.mojang.authlib.GameProfile
+import com.redefantasy.core.shared.misc.utils.ChatColor
 import com.redefantasy.core.shared.misc.utils.SequencePrefix
 import com.redefantasy.core.spigot.misc.player.sendPacket
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo
@@ -19,6 +20,11 @@ class PlayerList(
     size: Int = 80
 ) {
 
+    private val COLOR_CODES = arrayOf(
+        'a', 'b', 'c', 'd', 'e', 'f',
+        '0', '1', '2', '3', '4', '5',
+        '6', '7', '8', '9'
+    )
     private val SEQUENCE_PREFIX = SequencePrefix()
 
     private val PLAYERS = MutableList(size) {
@@ -30,9 +36,23 @@ class PlayerList(
             0,
             WorldSettings.EnumGamemode.NOT_SET,
             CraftChatMessage.fromString(
-                "§1"
+                this.generateHiddenString()
             )[0]
         )
+    }
+
+    private fun generateHiddenString(size: Int = 4): String {
+        val stringBuilder = StringBuilder()
+
+        for (i in 0..size) {
+            COLOR_CODES.shuffle()
+
+            stringBuilder.append(ChatColor.getByChar(
+                COLOR_CODES[i]
+            ))
+        }
+
+        return stringBuilder.toString()
     }
 
     companion object {
