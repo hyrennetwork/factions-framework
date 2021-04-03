c28fb19463a4f508bec0b1ee3a3ab2ff0feff359
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-03 02:06:51 -0300

    dev

--- src/main/kotlin/com/redefantasy/factions/framework/misc/tablist/PlayerList.kt
+++ src/main/kotlin/com/redefantasy/factions/framework/misc/tablist/PlayerList.kt
@@ -1,11 +1,13 @@
 package com.redefantasy.factions.framework.misc.tablist
 
 import com.mojang.authlib.GameProfile
-import com.redefantasy.core.shared.misc.utils.ChatColor
 import com.redefantasy.core.shared.misc.utils.SequencePrefix
 import com.redefantasy.core.spigot.misc.player.sendPacket
+import net.minecraft.server.v1_8_R3.ChatComponentText
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo
 import net.minecraft.server.v1_8_R3.WorldSettings
+import org.apache.commons.lang3.RandomStringUtils
+import org.bukkit.Bukkit
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer
 import org.bukkit.craftbukkit.v1_8_R3.util.CraftChatMessage
 import org.bukkit.entity.Player
@@ -14,59 +16,35 @@ import java.util.*
 /**
  * @author Gutyerrez
  */
-class PlayerList private constructor(
+class PlayerList(
     private val player: Player,
     size: Int = 80
 ) {
 
-    private val COLOR_CODES = arrayOf(
-        'a', 'b', 'c', 'd', 'e', 'f',
-        '0', '1', '2', '3', '4', '5',
-        '6', '7', '8', '9'
-    )
     private val SEQUENCE_PREFIX = SequencePrefix()
 
     private val PLAYERS = MutableList(size) {
         PacketPlayOutPlayerInfo.PlayerInfoData(
             GameProfile(
                 UUID.randomUUID(),
-                "__${SEQUENCE_PREFIX.next()}"
+                SEQUENCE_PREFIX.next()
             ),
             0,
-            WorldSettings.EnumGamemode.SURVIVAL,
+            WorldSettings.EnumGamemode.NOT_SET,
             CraftChatMessage.fromString(
-                this.generateHiddenString()
+                "§0${RandomStringUtils.randomAlphabetic(8)}"
             )[0]
         )
     }
 
-    private fun generateHiddenString(size: Int = 4): String {
-        val stringBuilder = StringBuilder()
-
-        for (i in 0..size) {
-            COLOR_CODES.shuffle()
-
-            stringBuilder.append(
-                ChatColor.getByChar(
-                    COLOR_CODES[i]
-                )
-            )
-        }
-
-        return stringBuilder.toString()
-    }
-
     companion object {
-
-        private val PLAYER_LIST = mutableMapOf<UUID, PlayerList>()
 
         const val CHANNEL_NAME = "hyren_custom_tab_list"
 
-        fun getPlayerList(player: Player) = PLAYER_LIST.getOrDefault(
-            player.uniqueId,
-            PlayerList(player)
-        )
+    }
 
+    init {
+        this.remove(player)
     }
 
     fun update(
@@ -75,28 +53,26 @@ class PlayerList private constructor(
     ) {
         val packet = PacketPlayOutPlayerInfo()
 
-        val playerInfoData = PacketPlayOutPlayerInfo.PlayerInfoData(
-            GameProfile(
-                UUID.randomUUID(),
-                SEQUENCE_PREFIX.next()
-            ),
-            0,
-            WorldSettings.EnumGamemode.NOT_SET,
-            CraftChatMessage.fromString(
-                text
-            )[0]
-        )
+        val playerInfoData = PLAYERS[index]
 
-        PLAYERS[index] = playerInfoData
+        val field = playerInfoData::class.java.getDeclaredField("e")
+
+        field.isAccessible = true
+
+        field.set(
+            playerInfoData,
+            ChatComponentText(text)
+        )
 
         packet.channels.add(CHANNEL_NAME)
 
+        packet.a = PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER
         packet.b = PLAYERS
 
         player.sendPacket(packet)
     }
 
-    fun removePlayer(player: Player) {
+    private fun remove(player: Player) {
         val packet = PacketPlayOutPlayerInfo()
 
         val gameProfile = (player as CraftPlayer).handle.profile
@@ -108,11 +84,12 @@ class PlayerList private constructor(
             null
         )
 
-        packet.a = PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER
+        packet.channels.add(CHANNEL_NAME)
 
+        packet.a = PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER
         packet.b.add(playerInfoData)
 
-        player.sendPacket(packet)
+        Bukkit.getOnlinePlayers().forEach { it.sendPacket(packet) }
     }
 
 }
