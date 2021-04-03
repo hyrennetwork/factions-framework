d8d791af9f2ee099094b6d72446a59cd092ff602
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-03 07:04:33 -0300

    dev

--- src/main/kotlin/com/redefantasy/factions/framework/echo/packet/listener/UserPunishedEchoPacketListener.kt
+++ src/main/kotlin/com/redefantasy/factions/framework/echo/packet/listener/UserPunishedEchoPacketListener.kt
@@ -4,6 +4,7 @@ import com.redefantasy.core.shared.echo.api.listener.EchoListener
 import com.redefantasy.core.shared.echo.packets.UserPunishedPacket
 import com.redefantasy.factions.framework.FactionsFrameworkConstants
 import com.redefantasy.factions.framework.FactionsFrameworkPlugin
+import org.bukkit.Bukkit
 import org.greenrobot.eventbus.Subscribe
 
 /**
@@ -19,28 +20,48 @@ class UserPunishedEchoPacketListener : EchoListener {
         val mPlayer = FactionsFrameworkPlugin.FACTIONS_API.getMPlayer(userId)
 
         if (mPlayer !== null) {
-            val method = mPlayer::class.java.getDeclaredMethod("getFaction")
+            val getName = mPlayer::class.java.superclass.getDeclaredMethod("getName")
 
-            method.isAccessible = true
+            getName.isAccessible = true
 
-            val faction = method.invoke(mPlayer) ?: return
+            val name = getName.invoke(mPlayer) as String
 
-            val _method = faction::class.java.superclass.superclass.getMethod("getId")
+            val getFaction = mPlayer::class.java.getDeclaredMethod("getFaction")
 
-            _method.isAccessible = true
+            getFaction.isAccessible = true
 
-            val factionId = _method.invoke(faction) ?: return
+            val faction = getFaction.invoke(mPlayer) ?: return
+
+            val getId = faction::class.java.superclass.superclass.getMethod("getId")
+
+            getId.isAccessible = true
+
+            val factionId = getId.invoke(faction) ?: return
 
             if (FactionsFrameworkConstants.IGNORED_FACTION_IDS.contains(factionId)) return
 
-            val __method = faction::class.java.getDeclaredMethod("getMPlayers")
+            val getMPlayers = faction::class.java.getDeclaredMethod("getMPlayers")
 
-            __method.isAccessible = true
+            getMPlayers.isAccessible = true
 
-            val mPlayers = __method.invoke(faction) as List<Any?>
+            val mPlayers = getMPlayers.invoke(faction) as List<Any?>
+
+            val economyClass = Class.forName("net.milkbowl.vault.economy.Economy")
+
+            val registeredServiceProvider = Bukkit.getServer().servicesManager.getRegistration(economyClass).provider
+
+            val getBalance = economyClass.getMethod("getBalance")
+
+            getBalance.isAccessible = true
 
             mPlayers.forEach {
-                println("MPlayer -> $it")
+                val balance = getBalance.invoke(registeredServiceProvider, name) as Double
+
+                println("$name -> Balanço: $balance")
+
+                var newBalance = balance - (30.0 * balance / 100.0)
+
+                println("$name -> Novo balanço: $newBalance")
             }
         }
     }
