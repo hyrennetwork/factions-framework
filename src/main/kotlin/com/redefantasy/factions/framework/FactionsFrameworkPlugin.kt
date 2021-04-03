46e25e4972b04a6550684139a973cb0c6395e0b3
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-03 02:11:14 -0300

    dev

--- src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
+++ src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
@@ -2,11 +2,12 @@ package com.redefantasy.factions.framework
 
 import com.redefantasy.core.spigot.CoreSpigotConstants
 import com.redefantasy.core.spigot.misc.plugin.CustomPlugin
-import com.redefantasy.core.spigot.misc.utils.PacketEvent
-import com.redefantasy.core.spigot.misc.utils.PacketListener
 import com.redefantasy.factions.framework.misc.tablist.PlayerList
-import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo
 import org.apache.commons.lang3.RandomStringUtils
+import org.bukkit.Bukkit
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
 
 /**
  * @author Gutyerrez
@@ -16,33 +17,32 @@ class FactionsFrameworkPlugin : CustomPlugin(false) {
     override fun onEnable() {
         super.onEnable()
 
-        CoreSpigotConstants.PROTOCOL_HANDLER.registerListener(
-            object : PacketListener() {
+        val pluginManager = Bukkit.getServer().pluginManager
 
-                override fun onSent(
-                    event: PacketEvent
+        pluginManager.registerEvents(
+            object : Listener {
+
+                @EventHandler
+                fun on(
+                    event: PlayerJoinEvent
                 ) {
                     val player = event.player
-                    val packet = event.packet
 
-                    if (packet is PacketPlayOutPlayerInfo && !packet.channels.contains(PlayerList.CHANNEL_NAME)) {
-                        event.isCancelled = true
+                    val playerList = PlayerList(player)
 
-                        val playerList = PlayerList(player)
+                    playerList.update(0, "§e§lMINHA FACÇÃO")
+                    playerList.update(1, "§e[STF] STAFF")
+                    playerList.update(2, "§1")
+                    playerList.update(3, "§a• §6[Master] #Gutyerrez")
+                    playerList.update(4, "§a• §6[Master] *ImRamon")
+                    playerList.update(5, "§7• [Master] +VICTORBBBBR")
 
-                        playerList.update(0, "§e§lMINHA FACÇÃO")
-                        playerList.update(1, "§e[STF] STAFF")
-                        playerList.update(2, "§1")
-                        playerList.update(3, "§a• §6[Master] #Gutyerrez")
-                        playerList.update(4, "§a• §6[Master] *ImRamon")
-                        playerList.update(5, "§7• [Master] +VICTORBBBBR")
-
-                        for (i in 6 until 16)
-                            playerList.update(i, "§7• -${RandomStringUtils.randomAlphabetic(8)}")
-                    }
+                    for (i in 6 until 16)
+                        playerList.update(i, "§7• -${RandomStringUtils.randomAlphabetic(8)}")
                 }
 
-            }
+            },
+            this
         )
     }
 
