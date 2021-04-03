340f08bbf44b6a63a6ad3228631421fe6e7147ca
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-02 23:54:57 -0300

    dev

--- src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
+++ src/main/kotlin/com/redefantasy/factions/framework/FactionsFrameworkPlugin.kt
@@ -6,6 +6,7 @@ import com.redefantasy.core.spigot.misc.utils.PacketEvent
 import com.redefantasy.core.spigot.misc.utils.PacketListener
 import com.redefantasy.factions.framework.misc.tablist.PlayerList
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo
+import org.apache.commons.lang3.RandomStringUtils
 import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -38,17 +39,9 @@ class FactionsFrameworkPlugin : CustomPlugin(false) {
                     playerList.update(3, "§a• §6[Master] #Gutyerrez")
                     playerList.update(4, "§a• §6[Master] *ImRamon")
                     playerList.update(5, "§7• [Master] +VICTORBBBBR")
-                    playerList.update(6, "§7• -Recruta")
-                    playerList.update(7, "§7• -Recruta")
-                    playerList.update(8, "§7• -Recruta")
-                    playerList.update(9, "§7• -Recruta")
-                    playerList.update(10, "§7• -Recruta")
-                    playerList.update(11, "§7• -Recruta")
-                    playerList.update(12, "§7• -Recruta")
-                    playerList.update(13, "§7• -Recruta")
-                    playerList.update(14, "§7• -Recruta")
-                    playerList.update(15, "§7• -Recruta")
-                    playerList.update(16, "§7• -Recruta")
+
+                    for (i in 6 until 16)
+                        playerList.update(i, "§7• -${RandomStringUtils.randomAlphabetic(8)}")
                 }
 
             },
