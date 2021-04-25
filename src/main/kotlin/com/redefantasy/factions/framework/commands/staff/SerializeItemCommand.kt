1c18d542b065dcb20a5fe1cf8b48ddfc58968ae3
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-25 16:46:01 -0300

    dev

--- src/main/kotlin/com/redefantasy/factions/framework/commands/staff/SerializeItemCommand.kt
+++ src/main/kotlin/com/redefantasy/factions/framework/commands/staff/SerializeItemCommand.kt
@@ -5,6 +5,8 @@ import com.redefantasy.core.shared.commands.restriction.entities.implementations
 import com.redefantasy.core.shared.groups.Group
 import com.redefantasy.core.shared.users.data.User
 import com.redefantasy.core.spigot.command.CustomCommand
+import com.redefantasy.core.spigot.misc.utils.ItemBuilder
+import org.bukkit.Material
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
@@ -20,7 +22,17 @@ class SerializeItemCommand : CustomCommand("serialize"), GroupCommandRestrictabl
 	): Boolean {
 		commandSender as Player
 
-		val itemStack = commandSender.itemInHand
+		val itemStack = ItemBuilder(Material.DIAMOND_SWORD).name(
+				"Item teste"
+			).amount(
+			2
+			).lore(
+				arrayOf(
+					"hm"
+				)
+			).build()
+
+		commandSender.itemInHand = itemStack
 
 		val serialized = CoreConstants.JACKSON.writeValueAsString(itemStack)
 
